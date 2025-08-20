ba88eb58da42e8f59f8b65ece7ec69874bb9c90f
himanshu-arun-patwardhan <himanshu.arun.patwardhan@gmail.com>
2025-08-20 16:30:46 +0530

    [clean code writing]

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
         .library(
             name: "SwiftLogger",
             targets: ["SwiftLogger"]
-        ),
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftLogger"
-        ),
+        )
     ]
 )
